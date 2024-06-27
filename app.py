2a859959d0fe128c022c2902ca92d27112b8fbf2
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-06-28 00:36:10 +0300

    Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS,cross_origin
 import pickle
 import pandas as pd
